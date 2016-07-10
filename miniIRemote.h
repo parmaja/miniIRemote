7b2b701f85220a073aae65b5ded4e50110738eb2
Zaher Dirkey <zaherdirkey@yahoo.com>
2016-07-10 21:42:13 +0300

    Fix Direct reading port depend on digitalPin

--- miniIRemote.h
+++ miniIRemote.h
@@ -1,5 +1,5 @@
 /**
-*	miniIRemote, read IR sensor pulses reader return a code (uint32).
+*   miniIRemote, read IR sensor pulses reader return a code (uint32).
 *
 * @license   The MIT License (MIT) Included in this distribution
 *
@@ -15,12 +15,17 @@
 //I keep it for debugging propose 
 /*
 #define IRPort         PINB
-#define IRpin          3
-#define getPulse       (IRPort & _BV(IRpin))
+#define IRPin          3
+#define getPulse       (IRPort & _BV(IRPin))
 */
+
 //#define getPulse      digitalRead(irPin) //slow, not work
 
-#define getPulse      (digitalPinToPort(irPin) & digitalPinToBitMask(irPin))
+//DIRECT_PIN_READ ported from https://github.com/PaulStoffregen/Encoder/blob/master/utility/direct_pin_read.h#L7
+
+#define DIRECT_PIN_READ(base, mask) (((*(base)) & (mask)) ? 1 : 0)
+
+#define getPulse      DIRECT_PIN_READ(irPort, digitalPinToBitMask(irDigitalPin))
 
 #define DEBUG //enable it to send out info over Serial, not work in attiny
 
@@ -52,149 +57,151 @@ namespace miniIR
 #define IR_LO_DATACOME 0
 #define IR_LONG_LENGTH 2500
 */
-	uint8_t irPin = -1;
-	
-	PROGMEM void init(uint8_t pin)
-	{
-		irPin = pin;		
-		pinMode(irPin, INPUT); // Set IR pin as input
-	}
-	
-	#ifdef DEBUG
-	uint16_t maxLength = 0;
-	uint16_t minLength = 0;
-	#endif 
+    static uint8_t irDigitalPin = -1;
+	volatile uint8_t* irPort = 0;
+    
+    PROGMEM void init(uint8_t pin)
+    {
+        irDigitalPin = pin;        
+		irPort = portInputRegister(digitalPinToPort(irDigitalPin));
+        pinMode(irDigitalPin, INPUT); // Set IR pin as input
+    }
+    
+    #ifdef DEBUG
+    uint16_t maxLength = 0;
+    uint16_t minLength = 0;
+    #endif 
 
-	PROGMEM uint32_t Read()
-	{  
-	  uint16_t lo_pulse =0, hi_pulse =0;
-	  
-	  struct {
-		bool started = false;
-		uint32_t code = 0;
-		uint8_t count = 0; 
-		#ifdef DEBUG
-		bool b = false;
-		#endif
-		
-		void reset(){
-		  #ifdef DEBUG
-		  b = false;
-		  maxLength = 0;
-		  minLength = 0;
-		  #endif DEBUG
-		  
-		  count = 0;
-		  code = 0;
-		}
+    PROGMEM uint32_t Read()
+    {  
+      uint16_t lo_pulse =0, hi_pulse =0;
+      
+      struct {
+        bool started = false;
+        uint32_t code = 0;
+        uint8_t count = 0; 
+        #ifdef DEBUG
+        bool b = false;
+        #endif
+        
+        void reset(){
+          #ifdef DEBUG
+          b = false;
+          maxLength = 0;
+          minLength = 0;
+          #endif DEBUG
+          
+          count = 0;
+          code = 0;
+        }
 
-		void add(uint16_t pulse)
-		{   
-		  code = code << 1;
-		  if (pulse > IR_DATA_LENGTH){
-			code = code | 1;      
-		  }      
-		  count++;
-		   
-		  #ifdef DEBUG
-		  if (pulse > 25) {
-			if (!b || (maxLength < pulse))
-			  maxLength = pulse;
-			if (!b || (minLength > pulse))
-			  minLength = pulse;
-			b = true;
-		  }
-		  #endif DEBUG
-		}
-	  } data;
+        void add(uint16_t pulse)
+        {   
+          code = code << 1;
+          if (pulse > IR_DATA_LENGTH){
+            code = code | 1;      
+          }      
+          count++;
+           
+          #ifdef DEBUG
+          if (pulse > 25) {
+            if (!b || (maxLength < pulse))
+              maxLength = pulse;
+            if (!b || (minLength > pulse))
+              minLength = pulse;
+            b = true;
+          }
+          #endif DEBUG
+        }
+      } data;
 
-	/*
-	 * Starting loop to collect pulses 
-	 * First pulse it ignored, it was high, waiting forever
-	 * after lo pulse come usually it is long low pulse, used to sign of data comming
-	 * second high maybe it is a long for data comming sign too, we can ignore it, sony not need it 
-	 * 
-	 */
-	  data.reset();
-	  
-	  while (data.count <= IR_PULSE_COUNT)
-	  {
-		#ifdef VUSB
-		usbPoll(); 
-		#endif
+    /*
+     * Starting loop to collect pulses 
+     * First pulse it ignored, it was high, waiting forever
+     * after lo pulse come usually it is long low pulse, used to sign of data comming
+     * second high maybe it is a long for data comming sign too, we can ignore it, sony not need it 
+     * 
+     */
+      data.reset();
+      
+      while (data.count <= IR_PULSE_COUNT)
+      {
+        #ifdef VUSB
+        usbPoll(); 
+        #endif
 
-		hi_pulse = 0;
-		while (getPulse) { //Wait for ever for the first pulse
-		  hi_pulse++;
-		  delayMicroseconds(IR_RESOLUTION);      
-		  if (hi_pulse >= IR_LONG_LENGTH)
-		  {
-			if (data.count > 0)
-			{
-			  #ifdef DEBUG
-			  Serial.print("Long Length");        
-			  #endif DEBUG
-			  if (!data.started)
-				return 0;
-			  else 
-			  {
-				#ifdef DEBUG
-				Serial.print(" Count= ");
-				Serial.println(data.count);
-				#endif DEBUG
-				return data.code;
-			  }
-			}
-			else if (hi_pulse >= IR_MAX_LENGTH) 
-			{
-			  hi_pulse = 0;
-			  #ifdef VUSB
-			  usbPoll(); 
-			  #endif
-			  //continue;
-			}
-		  }
-		}
-		lo_pulse = 0;
-		while (!getPulse) {
-		  lo_pulse++;
-		  delayMicroseconds(IR_RESOLUTION);
-		  if ((lo_pulse >= IR_MAX_LENGTH)) {
-			#ifdef DEBUG
-			Serial.println(" lo pulse IR_MAX_LENGTH ");        
-			#endif DEBUG
-			#ifdef VUSB
-			usbPoll(); 
-			#endif
-			
-			return 0; //too long data length
-		  }
-		}
+        hi_pulse = 0;
+        while (getPulse) { //Wait for ever for the first pulse
+          hi_pulse++;
+          delayMicroseconds(IR_RESOLUTION);      
+          if (hi_pulse >= IR_LONG_LENGTH)
+          {
+            if (data.count > 0)
+            {
+              #ifdef DEBUG
+              Serial.print("Long Length");        
+              #endif DEBUG
+              if (!data.started)
+                return 0;
+              else 
+              {
+                #ifdef DEBUG
+                Serial.print(" Count= ");
+                Serial.println(data.count);
+                #endif DEBUG
+                return data.code;
+              }
+            }
+            else if (hi_pulse >= IR_MAX_LENGTH) 
+            {
+              hi_pulse = 0;
+              #ifdef VUSB
+              usbPoll(); 
+              #endif
+              //continue;
+            }
+          }
+        }
+        lo_pulse = 0;
+        while (!getPulse) {
+          lo_pulse++;
+          delayMicroseconds(IR_RESOLUTION);
+          if ((lo_pulse >= IR_MAX_LENGTH)) {
+            #ifdef DEBUG
+            Serial.println(" lo pulse IR_MAX_LENGTH ");        
+            #endif DEBUG
+            #ifdef VUSB
+            usbPoll(); 
+            #endif
+            
+            return 0; //too long data length
+          }
+        }
 
-		if (IR_LO_DATACOME > 0)
-		{
-			if (!data.started) {
-			  data.reset();
-			  data.started = true;
-			}
-			if ((lo_pulse >= IR_LO_DATACOME))
-			  continue;
-		}
-		else {
-			if (!data.started) {
-			  data.reset();
-			  data.started = true;
-			}
-		}
-		
-		if (data.started) {
-		  data.add(hi_pulse + lo_pulse);
-		}
-	  }
+        if (IR_LO_DATACOME > 0)
+        {
+            if (!data.started) {
+              data.reset();
+              data.started = true;
+            }
+            if ((lo_pulse >= IR_LO_DATACOME))
+              continue;
+        }
+        else {
+            if (!data.started) {
+              data.reset();
+              data.started = true;
+            }
+        }
+        
+        if (data.started) {
+          data.add(hi_pulse + lo_pulse);
+        }
+      }
 
-	#ifdef DEBUG
-	  Serial.println("Exit");
-	#endif
-	  return data.code;
-	}
+    #ifdef DEBUG
+      Serial.println("Exit");
+    #endif
+      return data.code;
+    }
 } //end of namespace miniIR
